80573e3a027aa6811a73313f2bd0ca8aa934c235
MxmDnn <mxmjuk@gmail.com>
2024-07-12 17:30:53 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string=input('Введите имя жены Ивана Царевича: ')
+print('Герои наших детских сказок Иван Царевич и',my_string)
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
